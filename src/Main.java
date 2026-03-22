aa06ab6beb053a2e797775e819c4f149221bc2d5
jcgsTCS <titulosycursos.jcgs@gmail.com>
2026-03-22 20:24:53 +0100

    Actualización MAIN Menu

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("=== MENÚ PRINCIPAL ===");
+            System.out.println("1. Ver opción 1");
+            System.out.println("2. Ver opción 2");
+            System.out.println("3. Ver opción 3");
+            System.out.println("0. Salir");
+            System.out.print("Escoge una opción: ");
+
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Has seleccionado la opción 1.");
+                    break;
+                case 2:
+                    System.out.println("Has seleccionado la opción 2.");
+                    break;
+                case 3:
+                    System.out.println("Has seleccionado la opción 3.");
+                    break;
+                case 0:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+
+            System.out.println();
+        } while (opcion != 0);
+
+        sc.close();
     }
 }
